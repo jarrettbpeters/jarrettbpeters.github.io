4d6bc22d210183e30364a0ee3ed242ea6f881c22
Jarrett Peters <jarrettbpeters@gmail.com>
2016-03-13 14:27:51 -0400

    added scrollspy

--- profile.js
+++ profile.js
@@ -7,3 +7,9 @@ $('#myModal').on('shown.bs.modal', function () {
         });
 
     });
+
+$('body').scrollspy({ target: '#navbar' })
+$('[data-spy="scroll"]').each(function) (){
+	var $spy = $(this).scrollspy('refresh')
+});
+
